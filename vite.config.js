9b3dfff2fec90f8edd49fb03c226b9c90f047ff8
Gaurav-900 <worksforgauravsharma@gmail.com>
2026-01-04 14:27:38 +0530

    Update app branding and PWA icons

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      includeAssets: ['favicon.png', 'apple-touch-icon.png'],
       manifest: {
         name: 'Void Tracker',
         short_name: 'Void',
